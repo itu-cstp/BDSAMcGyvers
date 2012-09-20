feb183c2dc89bc40d8afa44ffae6fc8c3239d56d
Christoffer Pedersen <stofferpedersen@gmail.com>
2012-09-20 14:50:50 +0200

    implemented remove for queue

--- AS38/SchedulingBenchmarking/BenchmarkSystem.cs
+++ AS38/SchedulingBenchmarking/BenchmarkSystem.cs
@@ -8,9 +8,11 @@ namespace SchedulingBenchmarking
 {
     class BenchmarkSystem
     {
+
         Scheduler scheduler = new Scheduler();
         public event EventHandler<StateChangedEventArgs> StateChanged;
-        public String[] Status;  
+        public String[] Status;
+       
 
         static void Main(String[] args) 
         {
@@ -31,8 +33,8 @@ namespace SchedulingBenchmarking
         }
 
         public BenchmarkSystem()
-        {
-            
+        {  
+ 
         }
 
         /*** Methods ***/
@@ -83,6 +85,8 @@ namespace SchedulingBenchmarking
             Queue<Job> MediumQueue;
             Queue<Job> LongQueue;
 
+            HashSet<Job> removedJobs;
+
             internal Scheduler()
             {
                 ShortQueue = new Queue<Job>();
@@ -104,37 +108,80 @@ namespace SchedulingBenchmarking
                     LongQueue.Enqueue(job);
             }
 
+            /// <summary>
+            /// Method to get the newest job between the three queues. 
+            /// It does so by simoply querying for their respective newest job
+            /// </summary>
+            /// <todo> Could be a one-liner ?</todo>
+            /// <returns> The newest job </returns>
             private Job getNewestJob()
             {
-
+                /// Create an array of the three times
                 var timedJobs = new Job[] {ShortQueue.OrderBy(j => j.TimeAdded).Last(),
                                            MediumQueue.OrderBy(j => j.TimeAdded).Last(), 
                                            LongQueue.OrderBy(j => j.TimeAdded).Last() };
-
+                
+                /// Return the most recent of the previously found three values
                 return timedJobs.OrderBy(j => j.TimeAdded).Last();
             }
 
-            internal Job removeJob(Job job)
+            /// <summary>
+            /// "Remove a job" in the sense that we are not really removing anything at this stage.
+            /// Simply mark it as removed and check for it when popping
+            /// </summary>
+            /// <param name="job"> Job to remove</param>
+            internal void removeJob(Job job)
             {
-                int time = job.ExpectedRuntimeMinutes;
-
-                if (time < 30)
-                    return ShortQueue.Dequeue();
-
-                if (time <= 30 && time < 120)
-                    return MediumQueue.Dequeue();
-
-                if (time <= 120)
-                    return LongQueue.Dequeue();
-
-                return null;
+                removedJobs.Add(job);
             }
-
+            
+            /// <summary>
+            /// Function to "pop", return and remove the newest element
+            /// Do so by finding the newest job between the three queues. 
+            /// Check if we have marked that job for removal. If so do not return it, but instead return the next job
+            /// 
+            /// </summary>
+            /// <returns> The popped job or null if there's no job to return</returns>
             internal Job popJob()
             {
+                
                 Job newestJob = getNewestJob();
 
-                return removeJob(newestJob);
+                // Criteria for which queue to pop from
+                int time = newestJob.ExpectedRuntimeMinutes;
+
+                // Object that we will return
+                Job popped = null;
+
+                /*
+                 * Look at the time! And determine which queue is suitable
+                 */
+                if (time < 30)
+                    popped = ShortQueue.Dequeue();
+
+                if (time <= 30 && time < 120)
+                    popped = MediumQueue.Dequeue();
+
+                if (time <= 120)
+                    popped = LongQueue.Dequeue();
+
+                // Check if the popped job is actually a removed one. 
+                // If so we should remove the mark and recursively return the next job in line
+                if (removedJobs.Contains(popped))
+                {
+                    removedJobs.Remove(newestJob);
+                    return popJob();
+                }
+
+                return popped; // A job that might be null 
+            }
+
+            /// <summary>
+            /// Simple method to check whether the three queues are all empty
+            /// </summary>
+            /// <returns> Boolean if all queues are empty</returns>
+            internal bool Empty(){
+                return (ShortQueue.Count < 1 && MediumQueue.Count < 1 && LongQueue.Count < 1);
             }
 
         }
