6321acc782213538edc24b7852d3de5969f9cf35
Christoffer Pedersen <stofferpedersen@gmail.com>
2012-09-23 18:26:56 +0200

    tested job.process()

--- AS38/SchedulingBenchmarkingTest/JobTest.cs
+++ AS38/SchedulingBenchmarkingTest/JobTest.cs
@@ -6,11 +6,29 @@ namespace SchedulingBenchmarking
     [TestClass]
     public class JobTest
     {
+        Job job1;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            job1 = new Job((string[] arg) => { return arg.Length.ToString(); }, new Owner("tester"), 3, 35);
+        }
+
+        [TestMethod]
+        public void TestProcess()
+        {
+
+            string[] args = new string[]{"task1", "task2", "task3" };
+            
+            // Job1 should return the length of the supplied array as a string
+            Assert.AreEqual("3", job1.Process(args));
+            Assert.AreEqual("0", job1.Process(new string[]{}));
+        }
+
         [TestMethod]
         public void TestToString()
         {
-            Job job = new Job((string[] arg) => { return arg.Length.ToString(); }, new Owner("tester"), 3, 35);
-            Assert.AreEqual("Job added: 01-01-0001 00:00:00 owner: tester",job.ToString());
+            Assert.AreEqual("Job added: 01-01-0001 00:00:00 owner: tester",job1.ToString());
         }
     }
 }
